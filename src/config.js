866028643e5eac7b6cac4ec171b25bbf22a87a72
emddeveloper <emd.developer@gmail.com>
2025-09-26 00:19:20 +0530

    16-26092025 config url changes as Docker url changed

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 
 //MERN_LOCALHOST BASE URL
  let MernLocalhostURL= "http://localhost:5000/api/v1";
-//MERN https://saicrmapi.shop API
- let MernVPSURL_SK= "https://saicrmapi.shop/api/v1";
- //MERN https://solarcrmapi.com
- let MernVPSURL= "https://solarcrmapi.com/api/v1";
+
+ //MERN https://tdsolarbackendnode.solarcrmapi.com/  DOCKER API
+ let MernVPSURL= "https://tdsolarbackendnode.solarcrmapi.com/api/v1";
 
 const config = {
   //NODE SERVER URL
